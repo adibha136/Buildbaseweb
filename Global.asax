d27f1fd153cb3641a25beb1c59149ec0ecd9a47e
SasikalaIyyamperumal <Sasikala@techeva.co.in>
2024-12-13 13:03:05 +0530

    MVC file with inspection

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mark7.MvcApplication" Language="C#" %>
---
+++ Server.js
@@ -0,0 +1,26 @@
+﻿const express = require('express');
+const multer = require('multer');
+const ExifParser = require('exif-parser');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/analyze-image', upload.single('image'), (req, res) => {
+    const imagePath = req.file.path;
+
+    // Extract EXIF metadata using a library like exif-parser
+    const exifData = ExifParser.create(fs.readFileSync(imagePath)).parse();
+
+    // Extract make and model information from the EXIF data
+    const make = exifData.tags.Make;
+    console.log(make);
+    const model = exifData.tags.Model;
+    console.log(model);
+
+    // Respond with make and model information
+    res.json({ make, model });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
